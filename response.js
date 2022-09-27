640378a4c0a9892ff516544e2b1db9af03c7d38a
SukrutN <sukrutnadigotti@gmail.com>
2022-09-27 17:57:20 -0400

    Initialize grid

---
+++ response.js
@@ -0,0 +1,17 @@
+const grid = document.getElementById('grid');
+
+// initializes grid w/ event listeners
+function initGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size ** 2; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor);
+        gridElement.addEventListener('mousedown', changeColor);
+        grid.appendChild(gridElement);
+    }
+}
+
+console.log("bob")
