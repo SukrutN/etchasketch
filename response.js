f8606994145a02017268220c34435063c4690447
SukrutN <sukrutnadigotti@gmail.com>
2022-09-30 01:06:08 -0400

    Add events for buttons, slider, and color picker

--- response.js
+++ response.js
@@ -1,13 +1,48 @@
-const DEFAULT_SIZE = 16
-const DEFAULT_COLOR = '#333333'
-const DEFAULT_MODE = 'color'
+// declare constants
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color';
 
-let currentColor = DEFAULT_COLOR
-let currentSize = DEFAULT_SIZE
-let currentMode = DEFAULT_MODE
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+let currentMode = DEFAULT_MODE;
 
-
+// returns elements we want to edit in relation to the DOM
 const grid = document.getElementById('grid');
+const colorButton = document.getElementById('colorButton');
+const rainbowButton = document.getElementById('rainbowButton');
+const resetButton = document.getElementById('resetButton');
+const eraserButton = document.getElementById('eraserButton');
+const sizeValue = document.getElementById('sizeValue');
+const slider = document.getElementById('slider');
+const colorChoice = document.getElementById('colorChoice');
+
+// events that give interactivity
+colorChoice.oninput = (e) => setColor(e.target.value);
+slider.onchange = (e) => setSize(e.target.value);
+slider.onmousemove = (e) => changeSize(e.target.value);
+resetButton.onclick = () => resetGrid();
+rainbowButton.onclick = () => setMode('rainbow');
+eraserButton.onclick = () => setMode('eraser');
+colorButton.onclick = () => setMode('color');
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function setSize(newSize) {
+    currentSize = newSize;
+}
+
+function setMode(newMode) {
+    triggerButton(newMode);
+    currentMode = newMode;
+}
+
+function setColor(newColor) {
+    currentColor = newColor;
+}
+
 
 // initializes grid w/ event listeners
 function initGrid(size) {
@@ -23,24 +58,21 @@ function initGrid(size) {
     }
 }
 
-
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
-
-
+// adjusts how the divs are colored based on user selection
 function changeColor {
-    if (e.type === 'mouseover' && !mouseDown) return
+    if (e.type === 'mouseover' && !mouseDown) return;
     if (currentMode === 'rainbow') {
-        const r = Math.floor(Math.random * 256)
-        const g = Math.floor(Math.random * 256)
-        const b = Math.floor(Math.random * 256)
-        e.target.style.backgroundColor = `rgb(${r},${g},${b})`
+        const r = Math.floor(Math.random * 256);
+        const g = Math.floor(Math.random * 256);
+        const b = Math.floor(Math.random * 256);
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
     } else if (currentMode === 'color' ) {
-        e.target.style.backgroundColor = currentColor
+        e.target.style.backgroundColor = currentColor;
     } else if (currentMode === 'eraser') {
-        e.target.style.backgroundColor = '#FFFFFF'
+        e.target.style.backgroundColor = '#FFFFFF';
     }
 
 }
+
+
 
